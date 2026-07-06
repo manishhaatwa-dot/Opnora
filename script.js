332cece0269e6bb89dccc1e063b747106d83a497
manishhaatwa-dot <manishhaatwa@gmail.com>
2026-07-06 19:20:54 +0530

    Add project data array to script.js
    Added an array of project objects with titles, descriptions, images, links, and buttons.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-
+[
+  {
+    "title": "XtureAI",
+    "description": "AI Solutions, Website & Mobile App Development",
+    "image": "images/xtureai.jpg",
+    "link": "https://xtureai.com",
+    "button": "Visit Website"
+  },
+  {
+    "title": "Opnora",
+    "description": "Professional Website & App Development Company",
+    "image": "images/opnora.jpg",
+    "link": "https://opnora.com",
+    "button": "Visit Website"
+  },
+  {
+    "title": "AppCreatorPali",
+    "description": "Custom Business Website Solutions",
+    "image": "images/appcreatorpali.jpg",
+    "link": "https://appcreatorpali.xtureai.com",
+    "button": "Visit Website"
+  }
+]
